db85906875e557c3f76dc1fdf5ddf98b3f8a0e68
hodl-farm <hello@hodl.farm>
2020-03-28 15:57:04 +0800

    fix archive download

--- docker/polkadot-archive-downloader/polkadot-archive-downloader.sh
+++ docker/polkadot-archive-downloader/polkadot-archive-downloader.sh
@@ -5,7 +5,7 @@ set -e
 if [ -d /polkadot/.local/share/polkadot/chains/ksmcc3/db/ ]; then
     echo "Blockchain database already exists, no need to import, exiting"
     exit 0
-elif [ -z "$snapshot" ]; then
+elif [ -z "$ARCHIVE_URL" ]; then
     echo "No archive download url specified, exiting"
     exit 0
 else
@@ -13,7 +13,7 @@ else
     rm -rvf /polkadot/.local/share/polkadot
     mkdir -p /polkadot/.local/share/polkadot/chains/ksmcc3/
     snapshot=$(echo -n "$@")
-    echo "Will download $snapshot"
+    echo "Will download $ARCHIVE_URL"
     curl -L $snapshot | lz4 -d | tar -xvf - -C /polkadot/.local/share/polkadot/chains/ksmcc3/
     chmod -R 777 /polkadot/.local/
     chown -R 1000:1000 /polkadot/.local/
