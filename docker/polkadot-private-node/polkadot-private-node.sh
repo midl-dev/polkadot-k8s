0bfc85eee144bddbf21e3b68153e2540b24a2add
hodl-farm <hello@hodl.farm>
2020-03-28 15:27:17 +0800

    dns resolution of sentries from private node

--- docker/polkadot-private-node/polkadot-private-node.sh
+++ docker/polkadot-private-node/polkadot-private-node.sh
@@ -7,6 +7,8 @@ sentry_node_0_node_id=$(curl -H "Content-Type: application/json" -d '{"id":1, "j
 sentry_node_1_node_id=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_networkState"}' http://polkadot-sentry-node-1.polkadot-sentry-node:9933/ | jq -r '.result.peerId')
 
 /usr/local/bin/polkadot --validator --name "🐑 Hodl_dot_farm 🐑" --pruning=archive --wasm-execution Compiled \
-         --listen-addr=/ip4/10.0.0.1/tcp/30333 \
-         --reserved-nodes /ip4/polkadot-sentry-node-0.polkadot-sentry-node/tcp/30333/p2p/${sentry_node_0_node_id} \
-         --reserved-nodes /ip4/polkadot-sentry-node-1.polkadot-sentry-node/tcp/30333/p2p/${sentry_node_1_node_id}
+         --out-peers=2 \
+         --in-peers=2 \
+         --reserved-only \
+         --reserved-nodes /dnsaddr/polkadot-sentry-node-0.polkadot-sentry-node/tcp/30333/p2p/${sentry_node_0_node_id} \
+         --reserved-nodes /dnsaddr/polkadot-sentry-node-1.polkadot-sentry-node/tcp/30333/p2p/${sentry_node_1_node_id}
