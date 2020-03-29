f1ca12b50226779a934fd9fdef17142cd4dd8eca
hodl-farm <hello@hodl.farm>
2020-03-29 22:03:00 +0800

    fix ingress rules for sentries, disable telemetry

--- docker/polkadot-private-node/polkadot-private-node.sh
+++ docker/polkadot-private-node/polkadot-private-node.sh
@@ -10,5 +10,6 @@ sentry_node_1_node_id=$(curl -H "Content-Type: application/json" -d '{"id":1, "j
          --out-peers=2 \
          --in-peers=2 \
          --reserved-only \
+         --no-telemetry \
          --reserved-nodes /dns4/polkadot-sentry-node-0.polkadot-sentry-node/tcp/30333/p2p/${sentry_node_0_node_id} \
          --reserved-nodes /dns4/polkadot-sentry-node-1.polkadot-sentry-node/tcp/30333/p2p/${sentry_node_1_node_id}
