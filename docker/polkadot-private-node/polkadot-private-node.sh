6e036cc518695fb1d3870003f91fddad5b955353
hodl-farm <hello@hodl.farm>
2020-03-29 22:55:48 +0800

    start private node with old peer id when sentry nodes are not reachable

--- docker/polkadot-private-node/polkadot-private-node.sh
+++ docker/polkadot-private-node/polkadot-private-node.sh
@@ -1,10 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 set -x
 
-sentry_node_0_node_id=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_networkState"}' http://polkadot-sentry-node-0.polkadot-sentry-node:9933/ | jq -r '.result.peerId')
-sentry_node_1_node_id=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_networkState"}' http://polkadot-sentry-node-1.polkadot-sentry-node:9933/ | jq -r '.result.peerId')
+get_node_id() {
+    #Attempt to fetch node id from sentry peers...
+
+    curl_node_id=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_networkState"}' http://$1:9933/ | jq -r '.result.peerId')
+    if [ -z $curl_node_id ]; then
+        #Peer seems offline, getting peer id from persistent storage
+        if [ -f /polkadot/polkadot-k8s/$1 ]; then
+            printf $(cat /polkadot/polkadot-k8s/$1)
+            #Else, peer unreachable and no persistent record available so we return nothing and expect liveness check to fail
+        fi
+    else
+        if ! [ "$(cat /polkadot/polkadot-k8s/$1)" == "$curl_node_id" ]; then
+            # Putting node id in persistent storage if not there already
+            mkdir -p /polkadot/polkadot-k8s
+            printf $curl_node_id > /polkadot/polkadot-k8s/$1
+        fi
+        printf $curl_node_id
+    fi
+}
+
+sentry_node_0_node_id=$(get_node_id "polkadot-sentry-node-0.polkadot-sentry-node")
+sentry_node_1_node_id=$(get_node_id "polkadot-sentry-node-1.polkadot-sentry-node")
 
 /usr/local/bin/polkadot --validator --name "🐑 Hodl_dot_farm 🐑" --pruning=archive --wasm-execution Compiled \
          --out-peers=2 \
