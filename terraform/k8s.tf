169bee127629b6814ca14f94733c3c0eb71e5ea9
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-05-25 16:25:22 +0800

    split gcp and k8s manifest in preparation for existing-cluster model

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,25 +1,3 @@
-module "terraform-gke-blockchain" {
-  source = "../../terraform-gke-blockchain"
-  org_id = var.org_id
-  billing_account = var.billing_account
-  terraform_service_account_credentials = var.terraform_service_account_credentials
-  project = var.project
-  project_prefix = "polkadot"
-}
-
-# Query the client configuration for our current service account, which should
-# have permission to talk to the GKE cluster since it created it.
-data "google_client_config" "current" {
-}
-
-# This file contains all the interactions with Kubernetes
-provider "kubernetes" {
-  load_config_file = false
-  host             = module.terraform-gke-blockchain.kubernetes_endpoint
-  cluster_ca_certificate = module.terraform-gke-blockchain.cluster_ca_certificate
-  token = data.google_client_config.current.access_token
-}
-
 resource "kubernetes_secret" "polkadot_node_keys" {
   metadata {
     name = "polkadot-node-keys"
