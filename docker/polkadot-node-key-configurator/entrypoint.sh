28327036801541eabdbb9561396a3e70621a566e
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-08-10 10:55:18 -0700

    do not populate peer nodes if there aren't any

--- docker/polkadot-node-key-configurator/entrypoint.sh
+++ docker/polkadot-node-key-configurator/entrypoint.sh
@@ -16,12 +16,15 @@ chmod 400 /polkadot/k8s_local_node_key
 
 # write public keys for all peers in an env file, to be sourced by polkadot startup script
 local_peers=("${LOCAL_PEERS}")
-echo "--reserved-nodes " > /polkadot/k8s_local_peer_cmd
+rm -rvf /polkadot/k8s_local_peer_cmd
 for node in ${local_peers[@]}
 do
     # do not peer with myself
     if [ "${node}" != "${KUBERNETES_NAME_PREFIX}" ]
     then
+        if [ ! -f /polkadot/k8s_local_peer_cmd ]; then
+            echo "--reserved-nodes " > /polkadot/k8s_local_peer_cmd
+        fi
         echo "/dns4/${node}-private-node-0.${node}-private-node.${node}/tcp/30333/p2p/$(subkey inspect-node-key --file /polkadot-node-keys/$node) " >> /polkadot/k8s_local_peer_cmd
     fi
 done
--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -29,7 +29,6 @@ EOY
 }
 export -f build_container
 find ${path.module}/../docker -mindepth 1 -maxdepth 1 -type d -exec bash -c 'build_container "$0"' {} \; -printf '%f\n'
-#build_container ${path.module}/../docker/polkadot-node-key-configurator
 EOF
   }
 }
