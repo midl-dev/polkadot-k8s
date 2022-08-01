56dc1f2e69a52bf2f4f8763484b71f62600ebb0d
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-08-01 13:19:41 -0700

    add session key verificator

---
+++ polkadot-session-key-verificator/index.ts
@@ -0,0 +1,62 @@
+/* A script to verify session key.
+ * Copyright 2022 MIDL.dev
+
+ * All inputs come from environment variables:
+ * 
+ *  * NODE_ENDPOINT : the polkadot/kusama node rpc (localhost)
+ *  * STASH_ACCOUNT_ADDRESS: the address of the validator's stash
+ *  * STASH_ACCOUNT_ALIAS: an alias for your validator
+ *  * VOTE_REPO: the github repository where your vote choices are kept in yaml format, for example midl-dev/dotsama-votes
+ *
+ *
+ *  To run continously, put the following script in a cronjob.
+ *  See for reference: https://opensource.com/article/17/11/how-use-cron-linux
+ * */
+
+// Import the API
+import '@polkadot/types';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { WebClient } from '@slack/web-api';
+
+async function sendErrorToSlackAndExit(message: string) {
+  console.error(message);
+  if (process.env.SLACK_ALERT_TOKEN) {
+    const slackWeb = new WebClient(process.env.SLACK_ALERT_TOKEN!);
+    await slackWeb.chat.postMessage({ text: message, channel: process.env.SLACK_ALERT_CHANNEL! })
+  }
+  process.exit(1)
+}
+async function main() {
+  const provider = new WsProvider(`ws://${process.env.NODE_ENDPOINT}:9944`);
+  // Create our API
+  const api = await ApiPromise.create({ provider });
+
+
+  const stash_account: string = process.env.STASH_ACCOUNT_ADDRESS!;
+  const stash_alias = process.env.STASH_ACCOUNT_ALIAS; //optional
+  const chain = process.env.CHAIN;
+  // https://wiki.polkadot.network/docs/build-ss58-registry
+  const currentBlockNum = (await api.rpc.chain.getHeader()).number;
+
+  console.log("Polkadot Session Key Verificator by MIDL.dev");
+  console.log("Copyright 2022 MIDLDEV OU");
+  console.log("***");
+  console.log(`Chain:                         ${chain}`);
+  console.log(`Current block number:          ${currentBlockNum.toHuman()}`);
+  console.log(`Stash account address:         ${stash_account}`);
+  console.log(`Stash account alias:           ${stash_alias}`);
+  console.log(`Node RPC endpoint in use:      ${process.env.NODE_ENDPOINT}`);
+  let nextKeys = await api.query.session.nextKeys(stash_account);
+  console.log(`Node's next keys: ${nextKeys}`);
+  console.log(`Node's next keys in hex: ${nextKeys.toHex()}`);
+  let nodeHasKeys = await api.rpc.author.hasSessionKeys(nextKeys.toHex());
+  console.log(`Local node has the session keys necessary to validate: ${nodeHasKeys}`);
+  if (!nodeHasKeys) {
+    await sendErrorToSlackAndExit("Node ${stash_alias} does not have the session keys advertised on-chain in local storage.");
+  } else {
+    process.exit(0);
+  }
+
+}
+
+main().then(console.log).catch(console.error);
