60751e48a779e33e1d834895f669be07e5622f27
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-12-04 18:54:09 -0800

    force http 1.1 (for cloudflare r2)

--- polkadot-archive-downloader/entrypoint.sh
+++ polkadot-archive-downloader/entrypoint.sh
@@ -18,7 +18,7 @@ else
     echo "Did not find pre-existing data, importing blockchain"
     mkdir -p /polkadot/.local/share/polkadot/chains/${chain_dir}/
     echo "Will download $ARCHIVE_URL"
-    curl -o - -L $ARCHIVE_URL | lz4 -c -d - | tar -x -C /polkadot/.local/share/polkadot/chains/${chain_dir}
+    curl -o --http1.1 - -L $ARCHIVE_URL | lz4 -c -d - | tar -x -C /polkadot/.local/share/polkadot/chains/${chain_dir}
     chmod -R 777 /polkadot/.local/
     chown -R 1000:1000 /polkadot/.local/
 fi
